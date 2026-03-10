432d7ea326957c1632cd10f34dd70f484088fd0c
Jessye Pereira <jessyekessia16@gmail.com>
2026-03-09 21:27:50 -0300

    Finalizando o exercicio simples

--- mini-projeto/main.go
+++ mini-projeto/main.go
@@ -2,7 +2,47 @@ package main
 
 import (
 	"fmt"
+	"errors"
 )
+
+func estatisticas(numeros []int) (int, int, float64, error) {
+	if len(numeros) == 0 {
+		return 0, 0, 0, errors.New("lista vazia")
+	}
+
+	min := numeros[0]
+	max := numeros[0]
+	soma := 0
+
+	for _, v := range numeros {
+		soma += v
+	}
+
+	for _, v := range numeros {
+		if v < min {
+			min = v
+		}
+		if v > max {
+			max = v
+		}
+	}
+
+	media := float64(soma) / float64(len(numeros))
+
+	return min, max, media, nil
+}
+
+func divisao(a int, b int) (float64, error) {
+	if b == 0 {
+		return 0, errors.New("Não pode dividir por zero")
+	}
+
+	r := float64(a) / float64(b)
+
+	return r, nil
+}
+
+
 
 func main() {
 	var numeros []int
@@ -47,6 +87,33 @@ func main() {
 				}
 			}
 		
+		} else if n == 4 {
+			min, max, media, erro := estatisticas(numeros)
+
+			if erro != nil {
+				fmt.Println(erro)
+			} else {
+				fmt.Println("Mínimo:", min)
+				fmt.Println("Máximo:", max)
+				fmt.Printf("Média: %.2f\n", media)
+			}
+
+		} else if n == 5 {
+			var n1 int
+			var n2 int
+
+			fmt.Println("Digite um número: ")
+			fmt.Scanln(&n1)
+			fmt.Println("Digite um segundo número: ")
+			fmt.Scanln(&n2)
+
+			r, erro := divisao(n1, n1)
+
+			if erro != nil {
+				fmt.Println(erro)
+			} else {
+				fmt.Printf("Divisão: %.2f\n", r)
+			}
 		} else if n == 6 {
 			numeros = nil
 			fmt.Println("Lista limpa com sucesso!")
